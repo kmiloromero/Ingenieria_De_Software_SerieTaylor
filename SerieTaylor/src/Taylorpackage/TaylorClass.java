480312176056a9335608d29cc0b605d5c74301b3
kmiloromero <scamiloromerol@gmail.com>
2021-08-25 15:38:32 -0500

    Taylor Class creation
    Creation
    -packageTaylo r
    -TaylorClass

--- SerieTaylor/src/Taylorpackage/TaylorClass.java
+++ SerieTaylor/src/Taylorpackage/TaylorClass.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Mathpackage;
+package Taylorpackage;
 
 /**
  *
  * @author Romero
  */
-public class MathClass {
+public class TaylorClass {
     
-   
-
 }
