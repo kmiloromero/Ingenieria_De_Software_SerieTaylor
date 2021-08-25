2425360ce226e54db8dae306836bde9239c6295b
JohanstyaN <johan.canon-l@uniminuto.edu.co>
2021-08-25 14:38:33 -0500

    PruebaGuardado

--- SerieTaylor/src/Mainpackage/MainClass.java
+++ SerieTaylor/src/Mainpackage/MainClass.java
@@ -16,6 +16,7 @@ public class MainClass {
      */
     public static void main(String[] args) {
         System.out.println(" hola mundo ");    
+        System.out.println("hola 2");
                 
 // TODO code application logic here
     }
