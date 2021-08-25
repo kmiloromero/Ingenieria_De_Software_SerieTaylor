65fbb6f872a0a47792a770424d3ec721d376c4b9
kmiloromero <scamiloromerol@gmail.com>
2021-08-25 14:36:40 -0500

    MathClass Commit
    math package and Math Class creation

--- SerieTaylor/src/Mainpackage/MainClass.java
+++ SerieTaylor/src/Mainpackage/MainClass.java
@@ -14,6 +14,7 @@ public class MainClass {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         System.out.println(" hola mundo ");    
                 
